f4048390729ea74175db1b2384b493734721faed
cagataydonmez <cagatay.donmez@gmail.com>
2026-02-27 13:44:39 +0300

    Create social authentication helper functions

---
+++ frontend-classic/src/utils/socialAuth.js
@@ -0,0 +1,26 @@
+// socialAuth.js
+
+/**
+ * Helper functions for social authentication.
+ */
+
+/**
+ * Authenticate user with Google.
+ */
+export const authenticateWithGoogle = () => {
+    // Implement Google authentication logic here
+};
+
+/**
+ * Authenticate user with Facebook.
+ */
+export const authenticateWithFacebook = () => {
+    // Implement Facebook authentication logic here
+};
+
+/**
+ * Authenticate user with Twitter.
+ */
+export const authenticateWithTwitter = () => {
+    // Implement Twitter authentication logic here
+};
