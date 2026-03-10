f9998bf9d409184490183106299fbc1e1223ae7b
DKNeural <123507290+DKNeural@users.noreply.github.com>
2026-03-10 16:37:17 +0300

    Fix phase2 connection migration transaction markers

--- server/migrations/005_phase2_connections.down.sql
+++ server/migrations/005_phase2_connections.down.sql
@@ -1,4 +1,8 @@
+BEGIN;
+
 DROP INDEX IF EXISTS idx_connection_requests_sender_status;
 DROP INDEX IF EXISTS idx_connection_requests_receiver_status;
 DROP INDEX IF EXISTS idx_connection_requests_sender_receiver;
 DROP TABLE IF EXISTS connection_requests;
+
+COMMIT;
